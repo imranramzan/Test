17c6d8f0f452ef8fc4f00a366ec0599de551be0d
Imran Ramzan <i.ramzan@hotmail.com>
2014-01-02 11:43:51 +0500

    First Commmit

---
+++ MultiResTest/MultiResTest/ios/AppController.h
@@ -0,0 +1,20 @@
+//
+//  MultiResTestAppController.h
+//  MultiResTest
+//
+//  Created by ImranR on 12/4/13.
+//  Copyright __MyCompanyName__ 2013. All rights reserved.
+//
+
+@class RootViewController;
+
+@interface AppController : NSObject <UIAccelerometerDelegate, UIAlertViewDelegate, UITextFieldDelegate,UIApplicationDelegate> {
+    UIWindow *window;
+    RootViewController    *viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (nonatomic, retain) RootViewController *viewController;
+
+@end
+
---
+++ MultiResTest/MultiResTest/ios/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  MultiResTestAppController.h
+//  MultiResTest
+//
+//  Created by ImranR on 12/4/13.
+//  Copyright __MyCompanyName__ 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
